6b0d1c1e50940e12e86d245bad75865125886250
Alexandru Gutu <alex.s.gutu@gmail.com>
2016-04-23 18:08:37 -0400

    Fixed error where coordinates werent getting passed. Still need to display direction outline.

--- js/maps.js
+++ js/maps.js
@@ -4,22 +4,35 @@
 
 var initialLocation;
 var siberia = new google.maps.LatLng(60, 105);
-var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+var newyork = new google.maps.LatLng(10.00, -10.10);
+
 var browserSupportFlag =  new Boolean();
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 
 var mapDiv = document.getElementById('googleMap');
+
+
 function initializeMap() {
+    var mapOptions = {
+        center: {lat: 44.540, lng: -78.546},
+        zoom: 8
+    };
+
+    directionsDisplay = new google.maps.DirectionsRenderer();
+
+    var map = new google.maps.Map(mapDiv, mapOptions);
+
     if(navigator.geolocation) {
-        var browserSupportFlag = true;
+        browserSupportFlag = true;
         navigator.geolocation.getCurrentPosition(function(position) {
-            var initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
             map.setCenter(initialLocation);
         }, function() {
             handleNoGeolocation(browserSupportFlag);
         });
-    }
-    // Browser doesn't support Geolocation
-    else {
+    } else {
+        // Browser doesn't support Geolocation
         browserSupportFlag = false;
         handleNoGeolocation(browserSupportFlag);
     }
@@ -27,19 +40,50 @@ function initializeMap() {
     function handleNoGeolocation(errorFlag) {
         if (errorFlag == true) {
             alert("Geolocation service failed.");
-            var initialLocation = newyork;
+            initialLocation = newyork;
         } else {
             alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
             initialLocation = siberia;
         }
         map.setCenter(initialLocation);
     }
-    
-    var mapOptions = {
-        center: {lat: 44.540, lng: -78.546},
-        zoom: 8
-    };
-    var map = new google.maps.Map(mapDiv, mapOptions);
+    directionsDisplay.setMap(map);
+}
+
+google.maps.event.addDomListener(window, "load", initializeMap());
+
+document.getElementById("directionRequests").onsubmit = function() {
+    var startingLocation = document.getElementById("departure").value;
+    var endingLocation = document.getElementById("arrival").value;
+
+    calcRoute(startingLocation, endingLocation);
 
 }
-google.maps.event.addDomListener(window, "load", initializeMap());
+
+function calcRoute(start, end) {
+    var request = {
+        origin:start,
+        destination:end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+        }
+    });
+}
+
+function getCoordinates(result) {
+    var currentRouteArray = result.routes[0];  //Returns a complex object containing the results of the current route
+    var currentRoute = currentRouteArray.overview_path; //Returns a simplified version of all the coordinates on the path
+
+
+    obj_newPolyline = new google.maps.Polyline({ map: map }); //a polyline just to verify my code is fetching the coordinates
+    var path = obj_newPolyline.getPath();
+    for (var x = 0; x < currentRoute.length; x++) {
+        var pos = new google.maps.LatLng(currentRoute[x].kb, currentRoute[x].lb)
+        latArray[x] = currentRoute[x].kb; //Returns the latitude
+        lngArray[x] = currentRoute[x].lb; //Returns the longitude
+        path.push(pos);
+    }
+}
