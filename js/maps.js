4f950631f79aaad985882d2e84a3f305b42e3d40
Alexandru Gutu <alex.s.gutu@gmail.com>
2016-04-23 19:46:12 -0400

    Changed some shit.

--- js/maps.js
+++ js/maps.js
@@ -3,8 +3,8 @@
  */
 
 var initialLocation;
-var siberia = new google.maps.LatLng(60, 105);
-var newyork = new google.maps.LatLng(10.00, -10.10);
+var siberia = new google.maps.LatLng(40.0583, -74.4057);
+var newyork = new google.maps.LatLng(40.7589, -73.9851);
 
 var browserSupportFlag =  new Boolean();
 var directionsDisplay;
@@ -12,17 +12,8 @@ var directionsService = new google.maps.DirectionsService();
 
 var mapDiv = document.getElementById('googleMap');
 
-
 function initializeMap() {
-    var mapOptions = {
-        center: {lat: 44.540, lng: -78.546},
-        zoom: 8
-    };
-
     directionsDisplay = new google.maps.DirectionsRenderer();
-
-    var map = new google.maps.Map(mapDiv, mapOptions);
-
     if(navigator.geolocation) {
         browserSupportFlag = true;
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -47,43 +38,38 @@ function initializeMap() {
         }
         map.setCenter(initialLocation);
     }
+
+    var mapOptions = {
+        center: initialLocation,
+        zoom: 15
+    };
+
+    var map = new google.maps.Map(mapDiv, mapOptions);
     directionsDisplay.setMap(map);
+
 }
 
 google.maps.event.addDomListener(window, "load", initializeMap());
 
 document.getElementById("directionRequests").onsubmit = function() {
-    var startingLocation = document.getElementById("departure").value;
-    var endingLocation = document.getElementById("arrival").value;
-
-    calcRoute(startingLocation, endingLocation);
-
-}
-
-function calcRoute(start, end) {
+    var start = document.getElementById("departure").value;
+    var end = document.getElementById("arrival").value;
     var request = {
-        origin:start,
-        destination:end,
+        origin:siberia,
+        destination:newyork,
         travelMode: google.maps.TravelMode.DRIVING
     };
+    console.log("prior to request");
+    console.log(request);
     directionsService.route(request, function(result, status) {
+        alert(status);
         if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(result);
+
+            // new google.maps.DirectionsRenderer({
+            //     map: map,
+            //     directions: result
+            // });
         }
     });
-}
-
-function getCoordinates(result) {
-    var currentRouteArray = result.routes[0];  //Returns a complex object containing the results of the current route
-    var currentRoute = currentRouteArray.overview_path; //Returns a simplified version of all the coordinates on the path
-
-
-    obj_newPolyline = new google.maps.Polyline({ map: map }); //a polyline just to verify my code is fetching the coordinates
-    var path = obj_newPolyline.getPath();
-    for (var x = 0; x < currentRoute.length; x++) {
-        var pos = new google.maps.LatLng(currentRoute[x].kb, currentRoute[x].lb)
-        latArray[x] = currentRoute[x].kb; //Returns the latitude
-        lngArray[x] = currentRoute[x].lb; //Returns the longitude
-        path.push(pos);
-    }
 }
