a7f0be75fa2c1b128935be00b47350f4708ca7eb
Alexandru Gutu <alex.s.gutu@gmail.com>
2016-04-23 22:22:47 -0400

    Parsing JSON

--- js/maps.js
+++ js/maps.js
@@ -2,6 +2,11 @@
  * Created by alexandrugutu on 4/23/16.
  */
 
+
+// Toll 50 for I90
+var tollLat = 42.94892;
+var tollLng = -78.76393;
+
 var initialLocation;
 var siberia = new google.maps.LatLng(40.0583, -74.4057);
 var newyork = new google.maps.LatLng(40.7589, -73.9851);
@@ -84,15 +89,38 @@ function getCoordinates(result) {
     var currentRouteArray = result.routes[0];  //Returns a complex object containing the results of the current route
     var currentRoute = currentRouteArray.overview_path; //Returns a simplified version of all the coordinates on the path
 
-
     obj_newPolyline = new google.maps.Polyline({ map: map }); //a polyline just to verify my code is fetching the coordinates
     var path = obj_newPolyline.getPath();
     for (var x = 0; x < currentRoute.length; x++) {
-        var pos = new google.maps.LatLng(currentRoute[x].kb, currentRoute[x].lb)
+        var pos = new google.maps.LatLng(currentRoute[x].lat(), currentRoute[x].lng())
         latArray[x] = currentRoute[x].lat(); //Returns the latitude
         lngArray[x] = currentRoute[x].lng(); //Returns the longitude
-        console.log(currentRoute[x].lat());
-        console.log(currentRoute[x].lng());
+        // console.log(currentRoute[x].lat(), currentRoute[x].lng());
+        // console.log(currentRoute[x-1].lat(), currentRoute[x-1].lng());
+        // console.log("----------");
         path.push(pos);
     }
+
+    getToll();
 }
+
+function getToll()
+{
+    for (var x = 1; x < latArray.length ; x++)
+    {
+        var lattitude1 = latArray[x];
+        var longitude1 = lngArray[x];
+
+        var lattitude2 = latArray[x-1];
+        var longitude2 = lngArray[x-1];
+
+        if (lattitude1 < tollLat && lattitude2 > tollLat && longitude1 < tollLng && longitude2 > tollLng)
+        {
+            console.log("Worked bitches");
+        }
+    }
+}
+
+$.getJSON("/toll.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
